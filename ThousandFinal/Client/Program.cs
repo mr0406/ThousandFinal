85dad1209ba9cc1c4350d43255cea1ad5643848d
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-11-03 20:23:22 +0100

    Revert "Delete adding tabs"
    This reverts commit 2fea3db5e7fbfbf22d5a960888ba236be20ae597.

--- ThousandFinal/Client/Program.cs
+++ ThousandFinal/Client/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Blazored.Toast;
 
 namespace ThousandFinal.Client
 {
@@ -19,7 +18,6 @@ namespace ThousandFinal.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
--- ThousandFinal/Server/Startup.cs
+++ ThousandFinal/Server/Startup.cs
@@ -1,4 +1,3 @@
-using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
